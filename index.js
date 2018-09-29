e5576aa1b4fcf70914ec1008f8d7edca0b03bdf8
jesrah <jesrah@gmail.com>
2018-09-29 04:11:47 -0700

    Added alphabetical sorting and edited buttons and clickhandlers accordingly

--- index.js
+++ index.js
@@ -1,38 +1,59 @@
 $(function() {
-	 	$(".submit").submit(function(e) {              
-         e.preventDefault();
-         var query = $('input[name=wiki-query]').val()
-         // $.ajax({
-         //  	url:` https://en.wikipedia.org/w/api.php?action=query&titles=${query}&prop=images&imlimit=5&callback=?`,
-         //  	headers: {
-         //  		'Content-Type': 'application/json; charset=utf-8'
-         //  	},
-         //  	method: "GET",
-         //  	data: { titles: query, format: 'html' },
-         //  	dataType: 'jsonp',
-         //  	encode: true
-         //  })
-         //  .done((data) => {
-         //   		$(".placeholder").empty().append(data)
-         //  })
-     
-        $.ajax({
-	        method: "GET",
-	        url:`https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${query}&utf8=&format=json&origin=*`,
-	        // url: `http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=${query}&callback=?`,
-	        contentType: "application/json; charset=utf-8",
-	        // async: false,
-	        dataType: "json",
-	        error: (err) => {console.log(err)}
-	      })
-	      .done((data) => {
-	      	console.log(data);
-	      	// let markup = data.parse.text['*'];
-	      	// //allows setting of div 'blurb'
-	      	// let blurb = $('<div></div>').html(markup)
-	      	// //puts div in placeholder, then p in div with markup
-	      	// $('.placeholder').html($(blurb)).find('p')
-	      	// // $(".placeholder").empty().append('<p> Data: ' + JSON.parse(JSON.stringify(data)) + '</p>')
-	      })
+	var formData = false;
+
+ 	$(".submit").submit(function(e) {              
+		e.preventDefault();
+		var query = $('input[name=wiki-query]').val() 
+		var limit = 10;
+    $.ajax({
+      method: "GET",
+      // url:`https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${query}&utf8=&format=json&origin=*`,
+      url:`https://en.wikipedia.org/w/api.php?action=opensearch&search=${query}&limit=${limit}&format=json&origin=*`,
+      contentType: "application/json; charset=utf-8",
+      dataType: "json",
+      error: (err) => {console.log(err)}
     })
+    .done((data) => {
+    	formData = true;
+    	//if "least relevant" is selected, set variable to false, and reverse each data array. 
+    	let titles = data[1];
+    	let snippets = data[2];
+    	let urls = data[3];
+
+    	$(".list-group").empty();
+    	if (titles.length === 0) { 
+    		$(".list-group").append('<li class="list-group-item">' + 'No results. Try different keywords.' + '</li>');
+    		return;
+    	}
+  		for (let i = 0; i < limit; i++) {
+  			$(".list-group").append('<li class="list-group-item">' + '<h3>' + titles[i] + '</h3>' + '<br>' + snippets[i] + '<br>' + `<a href="${urls[i]}">` + urls[i] + '</a>' + '</li>')
+  		}
+  	})
+
+  	$('.sort-az-btn').click(() => {
+  		if (formData) {
+  			var list = $('.list-group');
+				var listItems = list.children('li').get();
+				listItems.sort((a, b) => {
+					var compA = $(a).text().toUpperCase();
+					var compB = $(b).text().toUpperCase();
+					return (compA < compB) ? - 1 : (compA > compB)? 1 : 0;
+				})
+				$.each(listItems, (index, item) => list.append(item))
+  		}
+  	})
+
+  	$('.sort-za-btn').click(() => {
+  		if (formData) {
+  			var list = $('.list-group');
+				var listItems = list.children('li').get();
+				listItems.sort((a, b) => {
+					var compA = $(a).text().toUpperCase();
+					var compB = $(b).text().toUpperCase();
+					return (compA > compB) ? - 1 : (compA < compB)? 1 : 0;
+				})
+				$.each(listItems, (index, item) => list.append(item))
+  		}  	
+  	})
+  })
 })
