2f74cd0160c1fe24486df5a804c870d5b422d952
Jessica Rahman <jesrah@gmail.com>
2018-10-25 18:17:06 -0700

    Cleaned up code, implemented ES6 conventions

--- index.js
+++ index.js
@@ -1,44 +1,51 @@
 $(function() {
-	var formData = false;
+	let formData = false;
 
  	$(".submit").submit(function(e) {              
 		e.preventDefault();
-		var query = $('input[name=wiki-query]').val() 
-		var limit = 10;
+		let query = $('input[id=query]').val();
+		const limit = 10;
     $.ajax({
       method: "GET",
       url:`https://en.wikipedia.org/w/api.php?action=opensearch&search=${query}&limit=${limit}&format=json&origin=*`,
-      contentType: "application/json; charset=utf-8",
+      contentType: "application/x-www-form-urlencoded; charset=utf-8",
       dataType: "json",
       error: (err) => {console.log(err)}
     })
     .done((data) => {
     	formData = true;
 
-    	let titles = data[1];
-    	let snippets = data[2];
-    	let urls = data[3];
+    	const [term, titles, snippets, urls] = data;
 
     	$(".list-group").empty();
     	if (titles.length === 0) { 
-    		$(".list-group").append('<li class="list-group-item">' + 'No results. Try typing just one keyword.' + '</li>');
+    		$(".list-group").append(`
+          <li class="list-group-item">No results. Try typing just one keyword.
+          </li>`);
     		return;
     	}
   		for (let i = 0; i < limit; i++) {
   			if (!urls[i]) {
   				return;
   			}
-  			$(".list-group").append('<li class="list-group-item">' + '<h3>' + titles[i] + '</h3>' + '<br>' + snippets[i] + '<br>' + `<a href="${urls[i]}">` + urls[i] + '</a>' + '</li>')
+  			$(".list-group").append(`
+          <li class="list-group-item">
+            <h3>${titles[i]}</h3>
+            <br>
+            ${snippets[i]}
+            <br>
+            <a href="${urls[i]}">${urls[i]}</a>
+          </li>`);
   		}
   	})
 
   	$('.sort-az-btn').click(() => {
   		if (formData) {
-  			var list = $('.list-group');
-				var listItems = list.children('li').get();
+  			let list = $('.list-group');
+				let listItems = list.children('li').get();
 				listItems.sort((a, b) => {
-					var compA = $(a).text().toUpperCase();
-					var compB = $(b).text().toUpperCase();
+					let compA = $(a).text().toUpperCase();
+					let compB = $(b).text().toUpperCase();
 					return (compA < compB) ? - 1 : (compA > compB)? 1 : 0;
 				})
 				$.each(listItems, (index, item) => list.append(item))
@@ -47,11 +54,12 @@ $(function() {
 
   	$('.sort-za-btn').click(() => {
   		if (formData) {
-  			var list = $('.list-group');
-				var listItems = list.children('li').get();
+  			let list = $('.list-group');
+				let listItems = list.children('li').get();
+        console.log(listItems)
 				listItems.sort((a, b) => {
-					var compA = $(a).text().toUpperCase();
-					var compB = $(b).text().toUpperCase();
+					let compA = $(a).text().toUpperCase();
+					let compB = $(b).text().toUpperCase();
 					return (compA > compB) ? - 1 : (compA < compB)? 1 : 0;
 				})
 				$.each(listItems, (index, item) => list.append(item))
